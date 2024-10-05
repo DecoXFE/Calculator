3393afffa3f419bb29b6f27251ca72df6aeac5ae
DecoPoxa <diegovalladolidlassalinas@gmail.com>
2024-10-05 14:12:19 +0200

    Intial functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +11,32 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'Calculator';
+  actualValue = '';
+  
+  onSelectNumber(buttonElement:any){
+    let value = buttonElement.textContent;
+    this.actualValue += value;
+    document.getElementById("display")?.setAttribute("value", this.actualValue);
+  }
+
+  onSelectClear(){
+    this.actualValue="";
+    document.getElementById("display")?.setAttribute("value","");
+  }
+
+  onSelectEquals(){
+    this.actualValue = eval(this.actualValue).toString();
+    document.getElementById("display")?.setAttribute("value",this.actualValue);
+    if(this.actualValue=="0"){
+      this.actualValue = ""
+    }
+  }
+
+  onSelectDelete(){
+    this.actualValue = this.actualValue.slice(0,-1);
+    document.getElementById("display")?.setAttribute("value",this.actualValue);
+  }
+
 }
